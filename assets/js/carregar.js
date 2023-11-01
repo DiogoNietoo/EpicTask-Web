162915a1215f84f753711a0877c469f6a00b9505
DiogoNieto <diogonieto563@gmail.com>
2023-11-01 17:08:15 -0300

    Finalizado

--- assets/js/carregar.js
+++ assets/js/carregar.js
@@ -15,18 +15,18 @@ function criarCard(tarefa){
     card.innerHTML = `
     <div class="card ${tarefa.concluida ? 'grey' : ''}">
              <div class="card-content">
-               <span class="card-title">Pedido: ${tarefa.titulo}</span>
-               <p>Descrição: ${tarefa.descricao}</p>
-               <span data-badge-caption="preco" class""badge blue white text">Preço: R$${tarefa.preco}</span>
+               <span class="card-title">Receita: ${tarefa.titulo}</span>
+               <p>Modo de Preparo: ${tarefa.descricao}</p>
+               <span data-badge-caption="preco" class""badge blue white text">Pontos: ${tarefa.preco}</span>
                <span><br>Status:</span>
                <span>${tarefa.concluida ? 'Concluido' : ''} </span>
                <i class="material-icons">${tarefa.concluida ? 'check' : ''} </span>
              </div>
              <div class="card-action">
-               <a href="#" class="btn black" onClick="apagar(${tarefa.id})">Remover
+               <a href="#" class="btn red" onClick="apagar(${tarefa.id})">Remover
                  <i class="material-icons">delete</i>
                </a>
-               <a href="#" class="btn red" onClick="concluir(${tarefa.id})">Aprovado
+               <a href="#" class="btn green" onClick="concluir(${tarefa.id})">Aprovado
                  <i class="material-icons">check</i>
                </a>
              </div>
